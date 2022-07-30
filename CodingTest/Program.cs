3d4fa8a51e48105909c02dd29ce172416109abe7
Shripad Shirsat <shripadshirsat@outlook.com>
2022-07-30 16:42:55 +0530

    Add project files.

---
+++ CodingTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CodingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "test";
+
+            char[] strarr = str.ToCharArray();
+
+            char[] revarr = new char[str.Length];
+
+            for(int i=0,j=str.Length-1; i<j; i++,j--)
+            {
+                strarr[i] = str[j];
+                strarr[j] = str[i];
+            }
+
+            string revstring = new string(strarr);
+
+
+            ///Palindrome check
+            ///
+            bool flag = false;
+            for(int i=0,j=str.Length-1; i<str.Length/2;i++,j--)
+            {
+                if (str[i] != str[j])
+                {
+                    flag = false;
+                    break;
+                }
+                else
+                    flag = true;
+            }
+
+          ///no of occurence of character in c
+
+        }
+    }
+}
